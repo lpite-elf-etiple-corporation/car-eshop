c1c79bfbed1e750cc1a308bdc8d78b4d7b41625b
lpite <farionsasa@gmail.com>
2022-02-11 12:55:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ const app = express();
 
 app.use(express.static("./static"));
 
-app.get("/", (req, res) => {
+function generatePage(page) {
   const header = fs.readFileSync("./static/header/header.html");
 
-  const main = fs.readFileSync("./static/main/main.html");
-
   const footer = fs.readFileSync("./static/footer/footer.html");
-
-  res.send(`
+  return `
   <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -28,50 +25,24 @@ app.get("/", (req, res) => {
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet"> 
       <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap" rel="stylesheet"> 
       <link rel="stylesheet" href="/style.css">
-
     </head>
     <body>
       ${header}
-      ${main}
+      ${page}
       ${footer}
     </body>
     <script src="https://unpkg.com/vue@3"></script>
     <script src="/renderCars.js"></script>
   </html>
-  `);
+  `;
+}
+
+app.get("/", (req, res) => {
+  const main = fs.readFileSync("./static/main/main.html");
+  res.send(generatePage(main));
 });
 app.get("/car/", (req, res) => {
-  const header = fs.readFileSync("./static/header/header.html");
-
   const car = fs.readFileSync("./static/car/car.html");
-
-  const footer = fs.readFileSync("./static/footer/footer.html");
-  res.send(`
-  <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap" rel="stylesheet"> 
-      <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet"> 
-      <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet"> 
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet"> 
-      <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap" rel="stylesheet"> 
-      <link rel="stylesheet" href="/style.css">
-
-    </head>
-    <body>
-      ${header}
-      ${car}
-      ${footer}
-    </body>
-    <script src="https://unpkg.com/vue@3"></script>
-    <script src="/renderCars.js"></script>
-  </html>
-  `);
+  res.send(generatePage(car));
 });
 app.listen(3000);
