f45c6d6ed1809d0606b606cfc3d803df3050619c
lpite <farionsasa@gmail.com>
2022-02-11 12:51:21 +0200

    фиксы

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
       <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet"> 
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet"> 
       <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap" rel="stylesheet"> 
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="/style.css">
 
     </head>
     <body>
@@ -36,7 +36,41 @@ app.get("/", (req, res) => {
       ${footer}
     </body>
     <script src="https://unpkg.com/vue@3"></script>
-    <script src="./renderCars.js"></script>
+    <script src="/renderCars.js"></script>
+  </html>
+  `);
+});
+app.get("/car/", (req, res) => {
+  const header = fs.readFileSync("./static/header/header.html");
+
+  const car = fs.readFileSync("./static/car/car.html");
+
+  const footer = fs.readFileSync("./static/footer/footer.html");
+  res.send(`
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap" rel="stylesheet"> 
+      <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet"> 
+      <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet"> 
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet"> 
+      <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap" rel="stylesheet"> 
+      <link rel="stylesheet" href="/style.css">
+
+    </head>
+    <body>
+      ${header}
+      ${car}
+      ${footer}
+    </body>
+    <script src="https://unpkg.com/vue@3"></script>
+    <script src="/renderCars.js"></script>
   </html>
   `);
 });
